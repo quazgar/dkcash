48d458f63cf31acc119e3fcc52d0f5f16f7591be
Daniel <daniel.hornung@gmx.de>
2018-01-09 23:45:32 +0100

    ENH: Test-creditor is now added, started work on account structure.

--- dkcash/db.py
+++ dkcash/db.py
@@ -1,15 +1,33 @@
-"""Binding functions and classes to the GnuCash database."""
+"""Binding functions and classes to the GnuCash database.
+
+Implementation notes
+--------------------
+
+Piecash requires SqlAlchemy classes to have a few special methods,
+e.g. `object_to_validate`.  This probably means that content similar to
+`sa_extra` module should be used, this is provided by the piecash_compat module.
+
+"""
 
 import os
+import uuid
 
 from IPython import embed
 
-from sqlalchemy import null
-from sqlalchemy.sql import expression  as sqe
+from sqlalchemy import (null,
+                        Column, ForeignKey,
+                        Boolean, DateTime, DECIMAL, Float, Integer, String,
+                        )
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import expression as sqe
 from sqlalchemy import schema as sch
 from sqlalchemy import types as sqt
 import piecash
 from piecash.core.account import AccountType
+
+from .piecash_compat import DeclarativeBase as PCCDeclarativeBase
 
 class DKDatabase:
     """Wrapper around the (modified) GnuCash database."""
@@ -24,48 +42,94 @@ class DKDatabase:
             self._add_sample_entries()
 
     def _enrich_database(self):
-        """Add the necessary extra tables if they don't exist yet."""
-        conn = self.book.session.connection()
+        """Add the necessary extra accounts and tables if necessary."""
+        self._init_accounts()
+        session = self.book.session
+        conn = session.connection()
         md = sch.MetaData(bind=conn, reflect=True)
-        cred_tab = sch.Table(
-            "creditors", md,
-            sch.Column('creditor_id', sqt.String, primary_key=True),
-            sch.Column('name', sqt.String, nullable=False),
-            sch.Column('address1', sqt.String, nullable=False),
-            sch.Column('address2', sqt.String),
-            sch.Column('address3', sqt.String),
-            sch.Column('address4', sqt.String),
-            sch.Column('phone', sqt.String),
-            sch.Column('email', sqt.String),
-            sch.Column('newsletter', sqt.Boolean, default=False,
-                       nullable=False),
+        Base = declarative_base(bind=conn, metadata=md)
+        class Creditor(Base):
+            __tablename__ = "creditors"
+            creditor_id = Column(String, primary_key=True)
+            name = Column(String, nullable=False)
+            address1 = Column(String, nullable=False)
+            address2 = Column(String)
+            address3 = Column(String)
+            address4 = Column(String)
+            phone = Column(String)
+            email = Column(String)
+            newsletter = Column(Boolean, default=False, nullable=False)
+
+        class Contract(Base):
+            __tablename__ = "contracts"
+            contract_id = Column(String, primary_key=True)
+            creditor = Column(ForeignKey("creditors.creditor_id"),
+                       nullable=False)
+            account = Column(ForeignKey("accounts.guid"), nullable=False)
+            date = Column(DateTime, nullable=False)
+            amount = Column(DECIMAL(2), nullable=False)
+            interest = Column(Float, nullable=False)
+            interest_payment = Column(String, nullable=False)
+            version = Column(String)
+            period_type = Column(String, nullable=False)
+            period_notice = Column(DateTime)
+            period_end = Column(DateTime)
+            cancellation_date = Column(DateTime, default=null())
+            active = Column(Boolean, nullable=False)
+
+        cred1 = Creditor(
+            creditor_id=str(uuid.uuid4()),
+            name="Newly newt",
+            address1="New Street 1",
+            address2="12345 Irgendwo",
+            newsletter=True
         )
-        contracts_tab = sch.Table(
-            "contracts", md,
-            sch.Column('contract_id', sqt.String, primary_key=True),
-            sch.Column('creditor', sch.ForeignKey("creditors.creditor_id"),
-                       nullable=False),
-            sch.Column('account', sch.ForeignKey("accounts.guid"),
-                       nullable=False),
-            sch.Column('date', sqt.DateTime, nullable=False),
-            sch.Column('amount', sqt.DECIMAL(2), nullable=False),
-            sch.Column('interest', sqt.Float, nullable=False),
-            sch.Column('interest_payment', sqt.String, nullable=False),
-            sch.Column('version', sqt.String),
-            sch.Column('period_type', sqt.String, nullable=False),
-            sch.Column('period_notice', sqt.DateTime),
-            sch.Column('period_end', sqt.DateTime),
-            sch.Column('cancellation_date', sqt.DateTime, default=null()),
-            sch.Column('active', sqt.Boolean, nullable=False),
-        )
-        md.create_all(conn.engine)
+
+        Base.metadata.create_all(conn.engine)
+
         print("DB enriched")
 
+    def _init_accounts(self):
+        acc = Account(name="My account",
+                      type="ASSET",
+                      parent=book.root_account,
+                      commodity=EUR,
+                      placeholder=True,)
+        subacc = Account(name="My sub account",
+                         type="BANK",
+                         parent=acc,
+                         commodity=EUR,
+                         commodity_scu=1000,
+                         description="my bank account",
+                         code="FR013334...",)
+
     def _add_sample_entries(self):
-        conn = self.book.session.connection()
-        md = sch.MetaData(bind=conn, reflect=True)
+        session = self.book.session
+        conn = session.connection()
+        Base = automap_base(declarative_base=PCCDeclarativeBase)
+        Base.prepare(conn.engine, reflect=True)
+
+        Creditor = Base.classes.creditors
+        Contract = Base.classes.contracts
         # Add a creditor and two contracts
-        cred1 = sqe.insert() # FIXME Doesn't work yet.
-        md.create_all(conn.engine)
+        cred1 = Creditor(
+            creditor_id=str(uuid.uuid4()),
+            name="Newly newt",
+            address1="New Street 1",
+            address2="12345 Irgendwo",
+            newsletter=True
+        )
+        embed()
+        contr1 = Contract(
+            contract_id=str(uuid.uuid4()),
+            creditor=cred1,
+        )
+        session.add(cred1)
+        session.commit()
+        embed()
+        # cred1 = sqe.insert(md) # FIXME Doesn't work yet.
+        # md.create_all(conn.engine)
         print("DB sample entries added")
+
+user = relationship("User", back_populates="addresses")
 
---
+++ dkcash/piecash_compat.py
@@ -0,0 +1,48 @@
+"""Compatibility tools for piecash.
+
+E.g. classes need some special methods.
+
+Code copied partly from piecash.
+"""
+
+from sqlalchemy.ext.declarative import as_declarative
+from sqlalchemy.orm import sessionmaker, object_session
+
+
+@as_declarative()
+class DeclarativeBase:
+    @property
+    def book(self):
+        """Return the gnc book holding the object
+        """
+        s = object_session(self)
+        return s and s.book
+
+    def object_to_validate(self, change):
+        """yield the objects to validate when the object is modified (change="new" "deleted" or "dirty").
+        For instance, if the object is a Split, if it changes, we want to revalidate not the split
+        but its transaction and its lot (if any). split.object_to_validate should yeild both split.transaction
+        and split.lot
+        """
+        return
+        yield
+
+    def validate(self):
+        """Always valid."""
+        pass
+
+    def get_all_changes(self):
+        try:
+            return self.book.session._all_changes[id(self)]
+        except KeyError:
+            return {"STATE_CHANGES": ["unchanged"],
+                    "OBJECT": self}
+
+    def __str__(self):
+        return self.__unirepr__()
+
+    def __repr__(self):
+        return self.__unirepr__()
+
+    def __unicode__(self):
+        return self.__unirepr__()
