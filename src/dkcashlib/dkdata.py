cb8f4fe9830ceebe3b2ff346f163454faf815773
Daniel <daniel@harvey>
2019-05-02 23:14:24 +0200

    WIP: Base account is found now.

--- src/dkcashlib/dkdata.py
+++ src/dkcashlib/dkdata.py
@@ -8,14 +8,53 @@ Because the same Sqlite GnuCash file is used for GnuCash specific operations
 opening and closing of the database connection on demand may be necessary.
 """
 
+import functools
+import inspect
 import os
+from warnings import warn
 
 import piecash
+
+_open_books = dict()
+
+# Possibly better implementation, as a class again:
+# https://stackoverflow.com/questions/30104047/how-can-i-decorate-an-instance-method-with-a-decorator-class
+def _book_open(func):
+    """Each book must be open only once at a time."""
+    @functools.wraps(func)
+    def wrap(self, *args, **kwargs):
+        # print("self: {self}\n*args: {args}\nkwargs: {kwargs}".format(
+        #     self=self, args=args, kwargs=kwargs))
+        # Get the normalized book filename.
+        filename = self._gnucash_file
+        filename = os.path.abspath(filename)
+
+        # Default case: Book was opened already
+        if filename in _open_books:
+            # print("    Not opening {} again for {}.".format(filename,
+            #                                             func.__name__))
+            kwargs.update({"book": _open_books[filename]})
+            result = func(self, *args, **kwargs)
+        else:
+            # Now the book is opened, the function called, then the book is
+            # saved and closed automatically.  Also the book is added to the
+            # list of open books and removed at the end.
+            with piecash.open_book(filename, readonly=False) as book:
+                # print("Opened {} for {}".format(filename, func.__name__))
+                _open_books[filename] = book
+                kwargs.update({"book": book})
+                result = func(self, *args, **kwargs)
+                # print("Saving and closing {}".format(filename))
+                book.save()
+                _open_books.pop(filename)
+        return result
+
+    return wrap
 
 class DKData:
     def __init__(self, gnucash_file="dkcash_data.sql", base_account=None):
         """Represents the DKCash data.
-        
+
 Parameters
 ----------
 gnucash_file : String, optional.
@@ -24,10 +63,9 @@ gnucash_file : String, optional.
 
 base_account : String, optional
     The base account where the "special" accounts should be created if they do
-    not yet exist.  This is a slash-separated string, for example
-    `/Aktiva/DKVerwaltung`.
+    not yet exist.  This is a colon-separated string, for example
+    `Aktiva:DKVerwaltung`.
 """
-        self._book = None
 
         self._gnucash_file = gnucash_file
         if not os.path.exists(gnucash_file):
@@ -41,33 +79,37 @@ base_account : String, optional
         """Creates the GnuCash file for this DKData object.
 
 The GnuCash file must not exist yet."""
-        piecash.create_book(self._gnucash_file)
-        
-    def _open_gnucash(self):
-        """Initialize the GnuCash database with the necessary accounts.
+        new_book = piecash.create_book(self._gnucash_file)
+        new_book.close()
 
-Also initializes a number of GnuCash specific attributes, which only are valid
-until the book is closed again.
-
-`self._book` is open after this method.
-        """
-        if not self._book:
-            self._book = piecash.open_book(self._gnucash_file)
-        def self._EUR = self._book.commodities.get(mnemonic="EUR")
+    @_book_open
+    def _init_gnucash(self, book=None):
+        """Initialize the GnuCash database with the necessary accounts."""
+        EUR = book.commodities.get(mnemonic="EUR")
         parent = self._get_base_account()
+        print(parent)
+        acc_direktkredite
 
     def _init_tables(self):
         """Initialize the GnuCash database with extra tables."""
+        print("_init_tables")
 
-    def _get_base_account(self):
+    @_book_open
+    def _get_base_account(self, book=None):
         """Returns the base account for this object.
-
-Requires an opened book.
 
 Returns
 -------
-out : The base account.
+out : The base account, or None if if there is no such account.
         """
-        if self._base_account is None:
-            return self._book.root_account
-        [x for x in self._base_account.split(os.path.sep) if len(x) > 0]
+        if self._base_account is None or len(self._base_account) == 0:
+            return book.root_account
+
+        try:
+            acc = [x for x in book.accounts
+                   if x.fullname == self._base_account][0]
+        except IndexError:
+            return None
+
+        # Base account found
+        return acc
